7d711f5fb03af380af067c23c557c09af4055cbf
santosh463 <santoshnaidu128@gmail.com>
2021-08-17 12:52:42 +0000

    cowin dashboard

---
+++ src/components/VaccinationByAge/index.js
@@ -0,0 +1,34 @@
+// Write your code here
+import {PieChart, Pie, Legend, Cell} from 'recharts'
+import {VaccinationByAgeContainer, Heading} from './styledComponents'
+
+const VaccinationByAge = props => {
+  const {vaccinationByAgeDetails} = props
+  return (
+    <VaccinationByAgeContainer>
+      <Heading>Vaccination by age</Heading>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="30%"
+          data={vaccinationByAgeDetails}
+          outerRadius="60%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#2d87bb" />
+          <Cell name="44-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#64c2ac" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{fontSize: 12, fontFamily: 'Roboto'}}
+        />
+      </PieChart>
+    </VaccinationByAgeContainer>
+  )
+}
+
+export default VaccinationByAge
---
+++ src/components/VaccinationCoverage/index.js
@@ -0,0 +1,71 @@
+// Write your code here
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
+
+import {VaccinationCoverageContainer, Heading} from './styledComponents'
+
+const VaccinationCoverage = props => {
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+  const {vaccinationCoverageDetails} = props
+
+  return (
+    <VaccinationCoverageContainer>
+      <Heading>Vaccination Coverage</Heading>
+      <BarChart
+        data={vaccinationCoverageDetails}
+        width={900}
+        height={400}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 1,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: '#6c757d',
+            strokeWidth: 0.5,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            paddingTop: 20,
+            textAlign: 'center',
+            fontSize: 12,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Bar
+          dataKey="dose1"
+          name="Dose 1"
+          fill="#5a8dee"
+          barSize="20%"
+          radius={[10, 10, 0, 0]}
+        />
+        <Bar
+          dataKey="dose2"
+          name="Dose 2"
+          fill="#f54394"
+          barSize="20%"
+          radius={[5, 5, 0, 0]}
+        />
+      </BarChart>
+    </VaccinationCoverageContainer>
+  )
+}
+
+export default VaccinationCoverage
